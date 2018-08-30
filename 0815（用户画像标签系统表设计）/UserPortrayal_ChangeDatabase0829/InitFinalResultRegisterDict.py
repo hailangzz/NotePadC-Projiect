6863fa7951dc822734b3a4dea9c60ea6e37ce9ba
hailangzz <344267342@qq.com>
2018-08-30 16:34:47 +0800

    2018-08-29,自动提取批次信息模块版本，待加入数据统计计算模块

---
+++ 0815（用户画像标签系统表设计）/UserPortrayal_ChangeDatabase0829/InitFinalResultRegisterDict.py
@@ -0,0 +1,59 @@
+import GlobalVariable as GV
+import copy
+# 说明：用于初始化，最终的覆盖人数记录插入，所需要的存储数据结果：
+
+def InitFinalResultRegisterDict():
+    if len(GV.ReceiveStandardDataList)==0:
+        print("标准化结果数据列表为空")
+        return
+    else:
+        for ReceiveStandardData in GV.ReceiveStandardDataList:
+            for FirstFloorKey in ReceiveStandardData.keys():
+                if FirstFloorKey=='MainClass':
+                    for SecondFloorKey in ReceiveStandardData[FirstFloorKey].keys():
+                        if SecondFloorKey not in GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass']:
+                            GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]=copy.deepcopy(GV.ClassifyMapDict)
+                            GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["TagClassifyName"]=SecondFloorKey.split('_')[0]
+                        for ThirdlyFloorKey in ReceiveStandardData[FirstFloorKey][SecondFloorKey].keys():
+                            if ThirdlyFloorKey=='ClassifyValue':
+                                if ThirdlyFloorKey not in GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"]:
+                                    GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey]={}
+                                for FourthlyFloorKey in ReceiveStandardData[FirstFloorKey][SecondFloorKey][ThirdlyFloorKey].keys():
+                                    if FourthlyFloorKey not in GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey]:
+                                        PersonNumber=ReceiveStandardData[FirstFloorKey][SecondFloorKey][ThirdlyFloorKey][FourthlyFloorKey]
+                                        GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey][FourthlyFloorKey]={"ClassifyValueMap":'',"PersonNumber":PersonNumber}
+
+                            elif ThirdlyFloorKey=='ChildClass':
+                                if ThirdlyFloorKey not in GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"]:
+                                    GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey]={}
+                                for FourthlyFloorKey in ReceiveStandardData[FirstFloorKey][SecondFloorKey][ThirdlyFloorKey].keys():
+                                    if FourthlyFloorKey not in GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey]:
+                                        GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey][FourthlyFloorKey]={}
+                                    for FifthFloorKey in ReceiveStandardData[FirstFloorKey][SecondFloorKey][ThirdlyFloorKey][FourthlyFloorKey]:
+                                        if FifthFloorKey == 'ClassifyValue':
+                                            if FifthFloorKey not in GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey][FourthlyFloorKey]:
+                                                GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey][FourthlyFloorKey][FifthFloorKey]={}
+                                            for SixthFloorKey in ReceiveStandardData[FirstFloorKey][SecondFloorKey][ThirdlyFloorKey][FourthlyFloorKey][FifthFloorKey]:
+                                                if SixthFloorKey not in GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey][FourthlyFloorKey][FifthFloorKey]:
+                                                    PersonNumber=ReceiveStandardData[FirstFloorKey][SecondFloorKey][ThirdlyFloorKey][FourthlyFloorKey][FifthFloorKey][SixthFloorKey]
+                                                    GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey][FourthlyFloorKey][FifthFloorKey][SixthFloorKey]={"ClassifyValueMap":'',"PersonNumber":PersonNumber}
+                                        elif FifthFloorKey == 'ChildClassTotal':
+                                            if FifthFloorKey not in GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey][FourthlyFloorKey]:
+                                                ChildClassTotalPersonNumber=ReceiveStandardData[FirstFloorKey][SecondFloorKey][ThirdlyFloorKey][FourthlyFloorKey][FifthFloorKey]
+                                                GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey][FourthlyFloorKey][FifthFloorKey] = {"ClassifyValueMap": '', "PersonNumber": ChildClassTotalPersonNumber}
+                                                #GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClass'][SecondFloorKey]["ClassifyValueDict"][ThirdlyFloorKey][FourthlyFloorKey][FifthFloorKey]={"ChildClassTotalName":FourthlyFloorKey+'汇总',"ClassifyValueMap":'',"PersonNumber":ChildClassTotalPersonNumber}
+
+
+                elif FirstFloorKey=='MainClassTotal':
+                    for SecondFloorKey in ReceiveStandardData[FirstFloorKey].keys():
+                        if SecondFloorKey not in GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClassTotal']:
+                            #print(SecondFloorKey)
+                            OriginSecondFloorKey=SecondFloorKey
+                            SecondFloorKey=SecondFloorKey+'_Total_Equal'
+                            #print(SecondFloorKey)
+                            PersonNumber=ReceiveStandardData[FirstFloorKey][OriginSecondFloorKey]
+                            GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClassTotal'][SecondFloorKey]=copy.deepcopy(GV.ClassifyMapDict)
+                            GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClassTotal'][SecondFloorKey]['TagClassifyName']=SecondFloorKey.split('_')[0]
+                            GV.FinalResultRegisterDict["ResultRegisterDict"]['MainClassTotal'][SecondFloorKey]['ClassifyValueDict'] = {"主标签汇总值":{"ClassifyValueMap":'',"PersonNumber":PersonNumber}}
+
+
---
+++ 0815（用户画像标签系统表设计）/UserPortrayal_ChangeDatabase0829/Main.py
@@ -0,0 +1,77 @@
+import GlobalVariable as GV
+import MySQLOperation as MSQLO
+import OperateClassifyValue as OCV
+import OperateCooperationCompany as OCC
+import OperateDataExtractBatch as ODEB
+import OperateTagClassify as OTC
+import QJC.OriginDataStatistics as ODS
+import InitFinalResultRegisterDict as IFRRD
+import FillTagClassifyMap as FTCM
+import FillClassifyValueMap as FCVM
+import FillDataExtractBatch as FDEB
+import InsertResultPersonNumber as IRPN
+import ExtractPublicTaskInfo as EPTI
+import copy
+import gc
+
+
+def main1():
+    #MysqlObject=MSQLO.Mysql('192.168.7.31',3306,'ngoss_dim','ngoss_dim')
+    MysqlObject = MSQLO.Mysql('127.0.0.1', 3306, 'root', 'mysql')
+    #print(MysqlObject._UseDatabase)
+    OriginDataPath=r'./QJC/test10000.txt'
+    GV.ReceiveStandardDataList=copy.deepcopy(ODS.OriginDataStatistics(OriginDataPath))
+    #print(GV.ReceiveStandardDataList)
+    IFRRD.InitFinalResultRegisterDict()
+    #print(GV.FinalResultRegisterDict)
+    #插入公司映射记录···
+    CompanyName=('广发银行信用卡')
+    OCC.InsertCooperationCompanyRegister(MysqlObject,CompanyName)
+    CompanyName1=('PPmoney')
+    OCC.InsertCooperationCompanyRegister(MysqlObject,CompanyName1)
+
+    BatchTuple=('广发银行信用卡','2018-08-27 08:23:45')
+    ODEB.InsertBatchRegister(MysqlObject,BatchTuple)
+    OTC.InsertTagClassifyRegister(MysqlObject)
+    FTCM.FillTagClassifyMap(MysqlObject)
+    OCV.InsertClassifyValueRegister(MysqlObject)
+    FCVM.FillClassifyValueMap(MysqlObject)
+    FDEB.FillDataExtractBatch(MysqlObject,'广发银行信用卡','2018-08-27 08:23:45')
+    #print(GV.FinalResultRegisterDict)
+    IRPN.InsertResultPersonNumber(MysqlObject)
+    EPTI.ExtractPublicTaskInfo(MysqlObject)
+    print(GV.ExtractPublicTaskInfoDict)
+
+def main()
+    # MysqlObject=MSQLO.Mysql('192.168.7.31',3306,'ngoss_dim','ngoss_dim')
+    MysqlObject = MSQLO.Mysql('127.0.0.1', 3306, 'root', 'mysql')
+    # print(MysqlObject._UseDatabase)
+    OriginDataPath = r'./QJC/test10000.txt'
+    GV.ReceiveStandardDataList = copy.deepcopy(ODS.OriginDataStatistics(OriginDataPath))
+    # print(GV.ReceiveStandardDataList)
+    IFRRD.InitFinalResultRegisterDict()
+    # print(GV.FinalResultRegisterDict)
+    # 插入公司映射记录···
+    CompanyName = ('广发银行信用卡')
+    OCC.InsertCooperationCompanyRegister(MysqlObject, CompanyName)
+    CompanyName1 = ('PPmoney')
+    OCC.InsertCooperationCompanyRegister(MysqlObject, CompanyName1)
+
+    BatchTuple = ('广发银行信用卡', '2018-08-27 08:23:45')
+    ODEB.InsertBatchRegister(MysqlObject, BatchTuple)
+    OTC.InsertTagClassifyRegister(MysqlObject)
+    FTCM.FillTagClassifyMap(MysqlObject)
+    OCV.InsertClassifyValueRegister(MysqlObject)
+    FCVM.FillClassifyValueMap(MysqlObject)
+    FDEB.FillDataExtractBatch(MysqlObject, '广发银行信用卡', '2018-08-27 08:23:45')
+    # print(GV.FinalResultRegisterDict)
+    IRPN.InsertResultPersonNumber(MysqlObject)
+    EPTI.ExtractPublicTaskInfo(MysqlObject)
+    print(GV.ExtractPublicTaskInfoDict)
+
+
+if __name__ == '__main__':
+    main()
+    gc.collect()
+    #print(GV.FinalResultRegisterDict)
+    print("over program!")
